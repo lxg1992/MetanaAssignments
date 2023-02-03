c356a154999d1a09d206a6e672dfbe23752aeed9
Alexey <lxg1992@gmail.com>
2023-02-03 18:32:08 -0500

    Added UT to PartialRefund

---
+++ Week4/contracts/PartialRefund.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PartialRefund is ERC20 {
+    address payable private _centralAuthority;
+    uint256 private tokenDecimals = 10**decimals();
+    uint256 private maxTokens = 1_000_000 * tokenDecimals;
+    bool public saleIsClosed = false;
+
+    constructor(address specialAddress) ERC20("MetanaToken", "MTK") {
+        _centralAuthority = payable(specialAddress);
+    }
+
+    modifier onlySpecialAddress() {
+        require(
+            msg.sender == _centralAuthority,
+            "Only special address can modify data"
+        );
+        _;
+    }
+
+    // Explicit setting of decimals to 18, although it's 18 by default
+    function decimals() public view virtual override returns (uint8) {
+        return 18;
+    }
+
+    function buy() external payable {
+        uint256 fullAmount = msg.value * 1000;
+        /**Technically, for the above, we need to divide it by (10 ** 18) (wei in ether)
+        and multiply by (10 ** 18) (decimal points in token), but since those numbers
+        are equal, it's skipped */
+        if (saleIsClosed) {
+            uint256 contractBalance = balanceOf(address(this));
+            if (contractBalance >= fullAmount) {
+                _transfer(address(this), msg.sender, fullAmount);
+            } else {
+                revert("Contract has less tokens available than required");
+            }
+        } else {
+            if (fullAmount + totalSupply() > maxTokens) {
+                revert("Supply will exceed maximum");
+            }
+            _mint(msg.sender, fullAmount);
+            if (totalSupply() >= maxTokens) {
+                saleIsClosed = true;
+            }
+        }
+    }
+
+    function withdrawEthereum(address payable to) public onlySpecialAddress {
+        uint256 amount = address(this).balance;
+        // require(amount > 0 wei, "Nothing to withdraw!");
+        (bool success, ) = to.call{value: amount}("");
+    }
+
+    function withdrawTokenToExternalAddress(address to)
+        public
+        onlySpecialAddress
+    {
+        _transfer(address(this), to, balanceOf(address(this)));
+    }
+
+    function sellBack(uint256 amount) external {
+        // Check token balance of user
+        require(
+            balanceOf(msg.sender) >= amount * tokenDecimals,
+            "Not enough tokens in balance"
+        );
+        // Check ether (in wei) balance of contract
+        require(
+            address(this).balance >= (amount * tokenDecimals) / 1000 / 2, /** or * 0.5 */
+            "Not enough ether in contract"
+        );
+        _transfer(msg.sender, address(this), amount * tokenDecimals);
+        uint256 amountToSendInWei = (amount * tokenDecimals) / 1000 / 2;
+        // Transfer of ether can commence as checks are succesful
+        (bool success, ) = payable(msg.sender).call{value: amountToSendInWei}(
+            ""
+        );
+    }
+}
---
+++ Week4/test/PartialRefund.js
@@ -0,0 +1,133 @@
+const {
+  time,
+  loadFixture,
+  setBalance,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const chai = require("chai");
+const BN = require("bn.js");
+const { ethers } = require("hardhat");
+chai.use(require("chai-bn")(BN));
+
+const { expect } = chai;
+
+describe("PartialRefund", function () {
+  let partialRefund;
+  let owner;
+  let nonOwner1;
+  let nonOwner2;
+  const saleClosingFn = () =>
+    partialRefund.buy({ value: ethers.utils.parseEther("1000") });
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  beforeEach(async () => {
+    [owner, nonOwner1, nonOwner2] = await ethers.getSigners();
+    const PartialRefund = await ethers.getContractFactory("PartialRefund");
+    partialRefund = await PartialRefund.deploy(owner.address);
+    await partialRefund.deployed();
+  });
+
+  describe("Deployment", function () {
+    it("Should have 18 decimal points", async () => {
+      expect(await partialRefund.decimals()).to.be.equal(18);
+    });
+
+    it("Should have sale open", async () => {
+      expect(await partialRefund.saleIsClosed()).to.be.false;
+    });
+  });
+
+  describe("Transactions", function () {
+    describe("Sale open", () => {
+      it("Should mint 1000 tokens with 1 ether", async () => {
+        await partialRefund.buy({ value: ethers.utils.parseEther("1") });
+        const balance = await partialRefund.balanceOf(owner.address);
+        expect(ethers.utils.formatUnits(balance, 18)).to.equal("1000.0");
+      });
+
+      it("Should fail to mint more than 1_000_000 tokens", async () => {
+        await expect(
+          partialRefund.buy({ value: ethers.utils.parseEther("1000.1") })
+        ).to.be.revertedWith("Supply will exceed maximum");
+      });
+    });
+
+    describe("Sale closed", () => {
+      it("Should close the sale once 1_000_000 tokens are bought", async () => {
+        // await partialRefund.buy({ value: ethers.utils.parseEther("1000") });
+        await saleClosingFn();
+        expect(await partialRefund.saleIsClosed()).to.be.true;
+      });
+
+      it("Should fail to buy tokens from contract with no balance", async () => {
+        await saleClosingFn();
+        // await expect (partialRefund.buy({ value: ethers.utils.parseEther("1000") }); // Closes the sale
+        await expect(
+          partialRefund.buy({ value: ethers.utils.parseEther("0.01") })
+        ).to.be.revertedWith(
+          "Contract has less tokens available than required"
+        );
+      });
+
+      it("Should buy tokens from contract balance", async () => {
+        await saleClosingFn();
+        await partialRefund.sellBack(1_000_000);
+        await partialRefund.buy({ value: ethers.utils.parseEther("0.5") });
+
+        expect(await partialRefund.balanceOf(partialRefund.address)).to.equal(
+          ethers.utils.parseEther("999500")
+        );
+      });
+
+      it("Should fail to sell back more tokens than held", async () => {
+        await saleClosingFn();
+        expect(await partialRefund.sellBack(1001)).to.be.revertedWith(
+          "Not enough tokens in balance"
+        );
+      });
+    });
+
+    describe("Withdraw", () => {
+      it("Ethereum: Should fail if unauthorized", async () => {
+        await expect(
+          partialRefund.connect(nonOwner1).withdrawEthereum(nonOwner1.address)
+        ).to.be.revertedWith("Only special address can modify data");
+      });
+
+      it("Token: Should fail if unauthorized", async () => {
+        await expect(
+          partialRefund
+            .connect(nonOwner1)
+            .withdrawTokenToExternalAddress(nonOwner1.address)
+        ).to.be.revertedWith("Only special address can modify data");
+      });
+
+      it("Should withdraw ethereum", async () => {
+        await saleClosingFn();
+        const beforeBalance = await ethers.provider.getBalance(
+          partialRefund.address
+        );
+        await expect(
+          await partialRefund.withdrawEthereum(owner.address)
+        ).to.changeEtherBalance(owner.address, beforeBalance);
+      });
+
+      it("Should withdraw tokens", async () => {
+        await saleClosingFn();
+        await partialRefund.sellBack(8000);
+        const beforeBalance = await partialRefund.balanceOf(nonOwner1.address);
+
+        await partialRefund.withdrawTokenToExternalAddress(nonOwner1.address);
+
+        expect(await partialRefund.balanceOf(owner.address)).to.equal(
+          ethers.utils.parseEther("992000")
+        );
+
+        expect(await partialRefund.balanceOf(nonOwner1.address)).to.equal(
+          ethers.utils.parseEther("8000")
+        );
+      });
+    });
+  });
+});
