b1c4a7b68c8658132ce86140fcbf2a11e2602a9c
Alexey <lxg1992@gmail.com>
2023-04-12 23:28:41 -0400

    Pub key

--- Week12/cte/contracts/PublicKey.sol
+++ Week12/cte/contracts/PublicKey.sol
@@ -4,7 +4,7 @@ contract PublicKeyChallenge {
     address owner = 0x92b28647Ae1F3264661f72fb2eB9625A89D88A31;
     bool public isComplete;
 
-    function authenticate(bytes publicKey) public {
+    function authenticate(bytes calldata publicKey) public {
         require(address(bytes20(keccak256(publicKey))) == owner);
 
         isComplete = true;
--- Week12/cte/test/PublicKey.js
+++ Week12/cte/test/PublicKey.js
@@ -1,5 +1,37 @@
 const eth = require("ethereumjs-util");
 const { expect } = require("chai");
+const { ec } = require("elliptic");
+
+function getSenderPublicKey(txData) {
+  // Extract the relevant values from the transaction data
+  const { from, r, s, v, chainId } = txData;
+
+  // Create an instance of the elliptic curve secp256k1
+  const curve = new ec("secp256k1");
+
+  // Prepare the transaction hash for signing
+  const txHash = `0x${txData.hash.slice(2)}${chainId
+    .toString(16)
+    .padStart(2, "0")}${r.slice(2)}${s.slice(2)}`;
+
+  // Convert the v value to an integer
+  const vValue = parseInt(v, 10);
+  console.log({ v, vValue });
+
+  // Determine the recovery parameter (y-parity) from the v value
+  const recoveryParam = vValue; //- 27;
+
+  // Derive the public key from the transaction hash and recovery parameter
+  const publicKey = curve.recoverPubKey(
+    Buffer.from(txHash, "hex"),
+    recoveryParam,
+    Buffer.from(r, "hex"),
+    Buffer.from(s, "hex")
+  );
+
+  // Convert the public key to uncompressed format and return it as a hexadecimal string
+  return `0x${publicKey.encode("hex", false)}`;
+}
 
 describe("Public Key", function () {
   it("should accept the correct public key for authenticate function", async () => {
@@ -14,10 +46,24 @@ describe("Public Key", function () {
       params: ["0x92b28647Ae1F3264661f72fb2eB9625A89D88A31"],
     });
 
+    await hre.network.provider.send("hardhat_setBalance", [
+      "0x92b28647Ae1F3264661f72fb2eB9625A89D88A31",
+      "0xDE0B6B3A7640000",
+    ]);
+
     const signer = await ethers.getSigner(
       "0x92b28647Ae1F3264661f72fb2eB9625A89D88A31"
     );
 
-    signer.sendTransaction();
+    const payload = {
+      to: "0x92b28647Ae1F3264661f72fb2eB9625A89D88A31",
+      gasLimit: "21000",
+    };
+
+    const txSent = await signer.sendTransaction(payload);
+    console.log({ txSent });
+
+    const pubKey = getSenderPublicKey(txSent);
+    console.log(pubKey);
   });
 });
