1065ef7b3c94143b8d96e4a672fd8292b13663b3
Alexey <lxg1992@gmail.com>
2023-06-14 20:35:13 -0400

    Added Thirdweb

--- Module13/oracleProject/frontend/src/main.jsx
+++ Module13/oracleProject/frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnectV1,
+  walletConnect,
+  safeWallet,
+  paperWallet,
+  magicLink,
+} from "@thirdweb-dev/react";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThirdwebProvider
+      activeChain="goerli"
+      autoConnect={true}
+      supportedWallets={[metamaskWallet(), coinbaseWallet(), walletConnect()]}
+    >
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
