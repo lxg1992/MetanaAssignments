1d5985fc630f0b9511868b92a9e1826b5d2d4d61
Alexey <lxg1992@gmail.com>
2023-08-20 16:54:31 -0400

    Added contracts

---
+++ Module17/dao/contracts/TimeLock.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+    constructor(
+        uint256 minDelay, //how long to wait before executing
+        address[] memory proposers, //list of addresses that can propose
+        address[] memory executors //list of addresses that can execute
+    ) TimelockController(minDelay, proposers, executors, address(0)) {}
+}
