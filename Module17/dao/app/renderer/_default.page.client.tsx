0a77169c6590b18eff96246f29451eb8011903bf
Alexey <lxg1992@gmail.com>
2023-09-04 20:12:44 -0400

    Linked with local

--- Module17/dao/app/renderer/_default.page.client.tsx
+++ Module17/dao/app/renderer/_default.page.client.tsx
@@ -3,6 +3,7 @@ export { render };
 import { hydrateRoot } from "react-dom/client";
 import { PageShell } from "./PageShell.js";
 import type { PageContextClient } from "./types.js";
+import { ethers } from "ethers";
 
 // This render() hook only supports SSR, see https://vite-plugin-ssr.com/render-modes for how to modify render() to support SPA
 async function render(pageContext: PageContextClient) {
@@ -11,11 +12,21 @@ async function render(pageContext: PageContextClient) {
     throw new Error(
       "Client-side render() hook expects pageContext.Page to be defined"
     );
+
+  let signer: ethers.Signer | undefined;
+  let address: string | undefined;
+  let provider: ethers.providers.Web3Provider | undefined;
+
+  if (typeof window !== "undefined" && window.ethereum) {
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    signer = provider.getSigner();
+    address = await signer.getAddress();
+  }
   const root = document.getElementById("react-root");
   if (!root) throw new Error("DOM element #react-root not found");
   hydrateRoot(
     root,
-    <PageShell pageContext={pageContext}>
+    <PageShell pageContext={{ ...pageContext }}>
       <Page {...pageProps} />
     </PageShell>
   );
